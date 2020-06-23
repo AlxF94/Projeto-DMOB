224e81b67f09e53a1f4aa5bc85b1a78e14b8d636
georgetonjr <37754623+georgetonjr@users.noreply.github.com>
2020-06-23 19:15:02 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Calc from  './src/pages/Calc';
+import Brows from  './src/pages/Brows';
+import Agend from  './src/pages/Agenda';
+
+
+export default function App() {
+  return (
+    <Calc/>,
+    <Brows/>,
+    <Agend/>
+  );
+}
+
